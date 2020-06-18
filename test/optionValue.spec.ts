539325763d5a8967347f3588c10c987817422e15
Jeremy Lorino <jeremy.lorino@stackwave.com>
2020-06-18 13:57:35 -0500

    better description.

--- test/optionValue.spec.ts
+++ test/optionValue.spec.ts
@@ -25,26 +25,28 @@ describe("Option Example", () => {
         expect(result).to.deep.equal({ kind: "some", value: "hello" });
     });
 
-    it("should not match", () => {
-        type Option<a = any> = { kind: "none" } | { kind: "some"; value: a };
-        const val = { kind: "some", value: "hello" };
+    describe("remove value type", () => {
+        it("should not match", () => {
+            type Option<a = any> = { kind: "none" } | { kind: "some"; value: a };
+            const val = { kind: "some", value: "hello" };
 
-        const result = match(val)
-            .with({ kind: "none" }, o => o.value)
-            .run();
+            const result = match(val)
+                .with({ kind: "none" }, o => o.value)
+                .run();
 
-        expect(result).to.be.undefined;
-    });
+            expect(result).to.be.undefined;
+        });
 
-    it("should evaluate the otherwise expression", () => {
-        type Option<a = any> = { kind: "none" } | { kind: "some"; value: a };
-        const val = { kind: "some", value: "hello" };
+        it("should evaluate the otherwise expression", () => {
+            type Option<a = any> = { kind: "none" } | { kind: "some"; value: a };
+            const val = { kind: "some", value: "hello" };
 
-        const result = match(val)
-            .with({ kind: "none" }, o => o.value)
-            .otherwise(() => "none")
-            .run();
+            const result = match(val)
+                .with({ kind: "none" }, o => o.value)
+                .otherwise(() => "none")
+                .run();
 
-        expect(result).to.equal("none");
+            expect(result).to.equal("none");
+        });
     });
 });
